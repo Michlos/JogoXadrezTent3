3c3045e157ef52593641d882c58dcf9b59f1084b
Michlos <michlos@gmail.com>
2020-11-28 20:44:05 -0300

    Criando a classe posição

--- JogoXadrezTent3/Program.cs
+++ JogoXadrezTent3/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
+using tabuleiro;
+
 namespace JogoXadrezTent3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: "+ p);
+            Console.ReadLine();
         }
     }
 }
