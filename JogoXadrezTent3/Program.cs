7d851515778b5585986c54647335bb1b5ce962ec
Michlos <michlos@gmail.com>
2020-11-28 21:29:31 -0300

    Começando a implementar as peças do tabuleiro

--- JogoXadrezTent3/Program.cs
+++ JogoXadrezTent3/Program.cs
@@ -2,6 +2,8 @@
 
 using tabuleiro;
 
+using xadrez;
+
 namespace JogoXadrezTent3
 {
     class Program
@@ -9,6 +11,10 @@ namespace JogoXadrezTent3
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
             Console.ReadLine();
