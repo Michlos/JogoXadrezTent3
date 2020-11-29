8c4002e13db9c4220e62e704909a7164f1252aed
Michlos <michlos@gmail.com>
2020-11-28 22:49:47 -0300

    Melhorando a implementação do método ImprimirTabuleiro

--- JogoXadrezTent3/Tela.cs
+++ JogoXadrezTent3/Tela.cs
@@ -10,18 +10,45 @@ namespace JogoXadrezTent3
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
-                    if (tab.peca(i, j) == null)
-                    {
-                        Console.Write("- ");
-                    }
-                    else
-                    {
-                        Console.Write(tab.peca(i, j) + " ");
-                    }
+                    imprimirPeca(tab.peca(i, j));
+                    Console.Write(" ");
+
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            ConsoleColor bgColor = Console.BackgroundColor;
+            ConsoleColor fgColor = Console.ForegroundColor;
+            if (peca == null)
+            {
+                Console.Write("-");
+            }
+            else
+            {
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(peca);
+                    Console.BackgroundColor = bgColor;
+                    Console.ForegroundColor = fgColor;
+                }
+                else if (peca.cor == Cor.Preta)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(peca);
+                    Console.BackgroundColor = bgColor;
+                    Console.ForegroundColor = fgColor;
+                }
+
             }
         }
     }
